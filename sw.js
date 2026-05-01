1207598b564ebb88e5d8d4900ac7397e2b8b2835
fresu431-droid <fresu431@gmail.com>
2026-05-01 09:56:26 +0200

    Update cache files path in service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-
 const CACHE = 'focustimer-v1';
-const FILES = ['/', '/index.html', '/manifest.json', '/icon.svg'];
+const FILES = ['/focustimer/', '/focustimer/index.html', '/focustimer/manifest.json'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
